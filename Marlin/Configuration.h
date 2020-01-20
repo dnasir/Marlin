2aef35e8d97b1a2f2e6f3e20920f93419c89d063
Dzulqarnain Nasir <dzul@geta.no>
2020-01-20 17:58:29 +0200

    Updated BLTouch offsets.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -917,7 +917,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 32, -2, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 36, -2, -2.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
